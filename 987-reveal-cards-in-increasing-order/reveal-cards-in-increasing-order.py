3983c9fca9fca3ece78d4fdfe672e7e51b1a6ad7
Anurag Singh <anuragsingh41245@gmail.com>
2026-08-10 19:38:31 +0530

    Time: 4 ms (13.15%) | Memory: 19.4 MB (67.23%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -2,17 +2,11 @@ class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
         n = len(deck)
         deck.sort()
-        
-        index_queue = deque(range(n))
-        result = [0] * n
-        
+        index_queue= deque(range(n))
+        result= [0] *n
         for card in deck:
-            # Reveal: assign the current smallest card to the front index
             pos = index_queue.popleft()
             result[pos] = card
-            
-            # Move next top card to the bottom (if any remain)
             if index_queue:
                 index_queue.append(index_queue.popleft())
-        
         return result
